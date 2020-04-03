8fdc708dbb2b115ace90d2f15d697dce394390b0
hubert.thieriot <ht@erprofiler.com>
2020-04-03 18:43:49 +0800

    - adding gadm1 & lombardia separated files

--- 00_prepare_input_lombardia.R
+++ 00_prepare_input_lombardia.R
@@ -1,36 +1,24 @@
+require(dplyr)
 
 
-lombardia.get_meas <- function(pollutants_names){
-  stations_url <- 'https://www.dati.lombardia.it/api/views/ib47-atvt/rows.csv?accessType=DOWNLOAD'
-  stations <- read.csv(url(stations_url)) %>% select(Idstazione, NomeTipoSensore, IdSensore, UnitaMisura, Idstazione, lat, lng)
+
+lombardia.get_stations <- function(){
+  file_metadata <- file.path(input_folder,'lombardia_stations.csv')
+  if(!file.exists(file_metadata)){
+    download.file(url='https://www.dati.lombardia.it/api/views/ib47-atvt/rows.csv?accessType=DOWNLOAD',
+                  destfile=file_metadata)
+  }
   
-  meas_url <- 'https://www.dati.lombardia.it/api/views/nicp-bhqi/rows.csv?accessType=DOWNLOAD'
-  meas <- read.csv(url(meas_url))
-  
-  meas_cleaned <- meas %>%
-    select(IdSensore, Data, Valore) %>% 
-    rename(
-      date=Data,
-      value=Valore
-    ) %>%
-    filter(value>0) %>%
-    mutate(date=lubridate::date(date)) %>%
-    group_by(IdSensore, date) %>%
-    summarise(value=mean(value, na.rm=T)) %>%
-    ungroup() %>%
-    left_join(stations) %>%
+  stations <- read.csv(file_metadata) %>%
+    dplyr::select(Idstazione, NomeTipoSensore, IdSensore, UnitaMisura, Idstazione, lat, lng) %>%
+    mutate_if(is.factor, as.character) %>%
     rename(
       station_id=Idstazione, 
       pollutant=NomeTipoSensore,
-      unit=UnitaMisura
-    ) %>%
-    select(-c(IdSensore)) %>%
-    group_by(station_id, pollutant, lat, lng) %>%
-    tidyr::nest() %>%
-    rename(meas=data)
-
-  # Translating pollutant name
-  unique(meas_cleaned$pollutant)
+      unit=UnitaMisura,
+      sensor_id=IdSensore
+    )
+  
   poll_corr <- list(
     NO2="Biossido di Azoto",
     SO2="Biossido di Zolfo",
@@ -46,6 +34,41 @@ lombardia.get_meas <- function(pollutants_names){
   )
   
   poll_df <- unlist(poll_corr)
-  meas_cleaned$pollutant <- names(poll_df)[match(meas_cleaned$pollutant, poll_df)]
-  return(meas_cleaned %>% filter(pollutant %in% pollutants_names))
+  stations$pollutant <- names(poll_df)[match(stations$pollutant, poll_df)]
+  stations %<>% filter(!is.na(pollutant))
+  return(stations)
+}
+
+lombardia.get_stations_sf <- function(){
+  sf::st_as_sf(lombardia.get_stations(), coords=c("lng","lat"), crs=4326)
+}
+
+lombardia.get_meas <- function(pollutants_names){
+  
+  stations <- lombardia.get_stations()
+  
+  meas_url <- 'https://www.dati.lombardia.it/api/views/nicp-bhqi/rows.csv?accessType=DOWNLOAD'
+  meas <- read.csv(url(meas_url))
+  
+  meas_cleaned <- meas %>%
+    select(IdSensore, Data, Valore) %>% 
+    rename(
+      sensor_id=IdSensore,
+      date=Data,
+      value=Valore
+    ) %>%
+    filter(value>0) %>%
+    mutate(date=lubridate::date(date)) %>%
+    group_by(IdSensore, date) %>%
+    summarise(value=mean(value, na.rm=T)) %>%
+    ungroup() %>%
+    left_join(stations) %>%
+    select(-c(sensor_id)) %>%
+    group_by(station_id, pollutant, lat, lng) %>%
+    tidyr::nest() %>%
+    rename(meas=data)
+
+  # Translating pollutant name
+  unique(meas_cleaned$pollutant)
+  meas_cleaned
 }
