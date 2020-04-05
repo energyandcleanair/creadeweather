d84ed9e62bd95aa1e0b5764fff8841cad69fa9d8
hubert.thieriot <ht@erprofiler.com>
2020-04-05 19:47:32 +0800

    - ability to train models of certain pollutants only - plotting results [wip]

--- 03_train_models.R
+++ 03_train_models.R
@@ -4,15 +4,26 @@ require(sf)
 require(tibble)
 require(lubridate)
 require(ggplot2)
+require(pbapply)
 source('99_utils.R')
 source('99_plot.R')
 
 exp_name <- 'lag7'
 test_frac <- 0.1
 day_lags <- c(1:7)
-n_regions = NULL #50 #You might not want to run every region / poll combination
+n_stations = 10 #50 #You might not want to run every region / poll combination
+pollutants = c('PM10')
 
 meas_weather <- readRDS('data/02_prep_training/output/meas_weather.RDS')
+
+if(!is.null(pollutants)){
+  meas_weather <- meas_weather %>% filter(pollutant %in% pollutants)
+}
+
+if(!is.null(n_stations)){
+  stations <- unique(meas_weather$station_id)[1:n_stations]
+  meas_weather <- meas_weather %>% filter(station_id %in% stations)
+}
 
 models <- list(model_gbm=function(training_data, formula){
     print("Training gbm")
@@ -69,7 +80,7 @@ plot.map_count(meas_weather_lag, folder=result_folder,
 # Train models
 #----------------
 # Split training / validation
-input_data <- meas_weather_lag %>% rowwise() %>%
+data <- meas_weather_lag %>% rowwise() %>%
   mutate(meas_weather = list(meas_weather %>% mutate(id = row_number()))) %>%
   mutate(training = list(dplyr::sample_frac(meas_weather, 1-test_frac))) %>% 
   mutate(testing = list(anti_join(meas_weather, training, by='id')))
@@ -77,24 +88,29 @@ input_data <- meas_weather_lag %>% rowwise() %>%
 # Adding models to tibble (each model is applied to each region, poll combination)
 model_names <- if (!is.null(names(models))) names(models) else seq_along(models)
 models_df <- tibble(model_name=names(models), model=models)
-input_data <- input_data %>% tidyr::crossing(models_df)
+data <- data %>% tidyr::crossing(models_df)
 
-n_poll = length(unique(meas_weather$pollutant))
-n_trainings = ifelse(!is.null(n_regions),n_regions * n_poll *length(models),nrow(input_data)) 
-output_data <- input_data[1:n_trainings,] %>%
-  mutate(model_fitted=purrr::map2(training, model, purrr::possibly(~.y(.x %>% select_at(formula_vars), formula), otherwise = NA, quiet = FALSE)))
+train_row <- function(idx){
+  tryCatch({
+    data[[idx,'model']](data[[idx,'training']] %>% select_at(formula_vars), formula)
+  }, error=function(err) NA)
+}
 
+models_fitted <- pblapply(seq(nrow(data)), train_row)
+data$model_fitted <- models_fitted
+models_fitted <- NULL
+                 
 #----------------
 # Predict
 #----------------
-output_data <- output_data %>%
+data <- data %>%
   mutate(training=purrr::map2(
     training, model_fitted,
     purrr::possibly(~ .x %>% mutate(predicted=predict(.y, .x),
                                     residuals=predicted-value),
                     otherwise = NA)))
 
-output_data <- output_data %>%
+data <- data %>%
   mutate(testing=purrr::map2(
     testing, model_fitted,
     purrr::possibly(~ .x %>% mutate(predicted=predict(.y, .x),
@@ -102,7 +118,7 @@ output_data <- output_data %>%
                     otherwise = NA)))
 
 # Also predict on whole dataset for plotting and residual analysis purposes
-output_data <- output_data %>%
+data <- data %>%
   mutate(meas_weather=purrr::map2(
     meas_weather, model_fitted,
     purrr::possibly(~ .x %>% mutate(predicted=predict(.y, .x),
@@ -113,27 +129,27 @@ output_data <- output_data %>%
 # Post compute
 #---------------
 rsq <- function(x,y) cor(x, y) ^ 2
-output_data <- output_data %>% mutate(rmse=purrr::map_dbl(training, purrr::possibly(~ Metrics::rmse(.x$value, .x$predicted), otherwise = NA)))
-output_data <- output_data %>% mutate(rmse_test=purrr::map_dbl(testing, purrr::possibly(~ Metrics::rmse(.x$value, .x$predicted), otherwise = NA)))
-output_data <- output_data %>% mutate(mae=purrr::map_dbl(training, purrr::possibly(~ Metrics::mae(.x$value, .x$predicted), otherwise = NA)))
-output_data <- output_data %>% mutate(mae_test=purrr::map_dbl(testing, purrr::possibly(~ Metrics::mae(.x$value, .x$predicted), otherwise = NA)))
-output_data <- output_data %>% mutate(rsq=purrr::map_dbl(training, purrr::possibly(~ rsq(.x$value, .x$predicted), otherwise = NA)))
-output_data <- output_data %>% mutate(rsq_test=purrr::map_dbl(testing, purrr::possibly(~ rsq(.x$value, .x$predicted), otherwise = NA)))
+data <- data %>% mutate(rmse=purrr::map_dbl(training, purrr::possibly(~ Metrics::rmse(.x$value, .x$predicted), otherwise = NA)))
+data <- data %>% mutate(rmse_test=purrr::map_dbl(testing, purrr::possibly(~ Metrics::rmse(.x$value, .x$predicted), otherwise = NA)))
+data <- data %>% mutate(mae=purrr::map_dbl(training, purrr::possibly(~ Metrics::mae(.x$value, .x$predicted), otherwise = NA)))
+data <- data %>% mutate(mae_test=purrr::map_dbl(testing, purrr::possibly(~ Metrics::mae(.x$value, .x$predicted), otherwise = NA)))
+data <- data %>% mutate(rsq=purrr::map_dbl(training, purrr::possibly(~ rsq(.x$value, .x$predicted), otherwise = NA)))
+data <- data %>% mutate(rsq_test=purrr::map_dbl(testing, purrr::possibly(~ rsq(.x$value, .x$predicted), otherwise = NA)))
 
 
 #-----------------------
 # Save and plot results
 #-----------------------
-saveRDS(output_data %>% dplyr::select(-c(training, testing)), file.path(result_folder, paste0(timestamp_str,'_results.RDS')))
+saveRDS(data %>% dplyr::select(-c(training, testing)), file.path(result_folder, paste0(timestamp_str,'_results.RDS')))
 
 
-plot.output_data(output_data, rolling_days=7,
+plot.output_data(data, rolling_days=7,
                               filepath=file.path(result_folder,paste0(timestamp_str,'_results_plotrows_7d.pdf')))
 
-plot.output_data(output_data, rolling_days=30,
+plot.output_data(data, rolling_days=30,
                  filepath=file.path(result_folder,paste0(timestamp_str,'_results_plotrows_30d.pdf')))
 
-plot.output_map(output_data, result_folder=result_folder, timestamp_str = timestamp_str,
+plot.output_map(data, result_folder=result_folder, timestamp_str = timestamp_str,
                 meas_col='rsq_test', title=expression(paste(R^{2}, " validation")))
 
 
@@ -148,19 +164,19 @@ average_over_yearly_periods <- function(tbl, meas_col, years, doys){
       summarise_at(c(meas_col), mean, na.rm = TRUE))[[meas_col]][[1]]
 }
 
-output_data <- output_data %>% rowwise() %>% 
+data <- data %>% rowwise() %>% 
   mutate(march_res_before=average_over_yearly_periods(meas_weather, 'residuals', years=c(2015:2019), doys=c(62:93)),
          march_res_after=average_over_yearly_periods(meas_weather, 'residuals', years=c(2020), doys=c(62:93)),
          value_mean=if(is.na(meas_weather)) NA else mean(meas_weather[['value']], na.rm = T)) %>%
   mutate(march_res_comparison = -(march_res_after - march_res_before)/value_mean,
          march_res_after_neg=(march_res_after/value_mean)*-1)
 
-output_data <- output_data %>% rowwise() %>%
+data <- data %>% rowwise() %>%
   mutate(march2020_value=average_over_yearly_periods(meas_weather, 'value', years=c(2020), doys=c(62:93)),
          march2020_predicted=average_over_yearly_periods(meas_weather, 'predicted', years=c(2020), doys=c(62:93))) %>%
   mutate(march2020_ratio=-1+march2020_value/march2020_predicted)
 
-output_data$march2020_ratio_c <- cut(output_data$march2020_ratio, labels=c("Decrease >100% (sic)",
+data$march2020_ratio_c <- cut(data$march2020_ratio, labels=c("Decrease >100% (sic)",
                                               "Decrease 20-100%",
                                               "Decrease 10-20%",
                                               "Decrease 5-10%",
@@ -173,15 +189,15 @@ output_data$march2020_ratio_c <- cut(output_data$march2020_ratio, labels=c("Decr
                                               ),
     breaks=c(-Inf,-1,-0.2,-0.1,-0.05,0,0.05,0.1,0.2,1,+Inf))
 
-output_data$march2020_ratio_c <- reorder(output_data$march2020_ratio_c, desc(output_data$march2020_ratio_c))
+data$march2020_ratio_c <- reorder(data$march2020_ratio_c, desc(data$march2020_ratio_c))
 
-plot.output_map(output_data, result_folder=result_folder, timestamp_str = timestamp_str,
+plot.output_map(data, result_folder=result_folder, timestamp_str = timestamp_str,
                 meas_col='march2020_ratio_c', title="March 2020 - Value vs Predicted",
                 scale=scale_fill_brewer(palette='RdBu', na.value="grey"),
                 labs=labs(fill=""))
 
 
-plot.output_map(output_data, result_folder=result_folder, timestamp_str = timestamp_str,
+plot.output_map(data, result_folder=result_folder, timestamp_str = timestamp_str,
                 meas_col='march_res_comparison', title="Relative March 2020 residuals vs March 2015-2019 residuals (blue: value is lower than predicted)",
                 scale=scale_fill_distiller(palette='RdBu', limits=c(-1,1), breaks=c(-1,-0.05,0,0.05,1), na.value="grey"))
 
