a924146a730cbbfaa9d5fc20a81a63793c8ca348
Hubert Thieriot <ht@erprofiler.com>
2020-04-18 09:38:24 +0000

    First steps using rmweather package

---
+++ 03_train_model_rmweather.R
@@ -0,0 +1,96 @@
+require(rmweather)
+require(dplyr)
+require(purrr)
+require(sf)
+require(tibble)
+require(lubridate)
+require(ggplot2)
+require(pbapply)
+require(pbmcapply)
+require(gbm)
+require(parallel)
+require(future)
+
+source('99_utils.R')
+source('99_plot.R')
+
+output_folder <- file.path('data', '03_train_models', 'output')
+if(!dir.exists(output_folder)) dir.create(output_folder, recursive = T)
+
+# Check we can find this file (to be copied later on)
+org_file <- if(rstudioapi::isAvailable()) rstudioapi::getActiveDocumentContext()$path else '03_train_models.R'
+
+
+exp_name <- 'lag7_no2_05deg_rmweather'
+day_lags <- c(1:7)
+n_stations = NULL #50 #You might not want to run every region / poll combination
+pollutants <-c('NO2') 
+
+meas_weather <- readRDS('data/02_prep_training/output/meas_w_weather_allpolls_05.RDS')
+# only keep those with values in 2020
+# meas_weather <- meas_weather %>% filter(max(meas_weather$date)>'2020-01-01')
+
+if(!is.null(pollutants)){
+  meas_weather <- meas_weather %>% filter(pollutant %in% pollutants)
+}
+
+if(!is.null(n_stations)){
+  stations <- unique(meas_weather$station_id)[1:n_stations]
+  meas_weather <- meas_weather %>% filter(station_id %in% stations)
+}
+
+weather_vars_available <- setdiff(colnames(meas_weather$meas_weather[[1]]), c('date','value'))
+weather_vars <- c('air_temp_min', 'air_temp_max', 'atmos_pres', 'wd', 'ws_max', 'ceil_hgt', 'precip', 'RH', 'pbl_min', 'pbl_max', 'sunshine')
+weather_vars_lags <- unlist(lapply(weather_vars, function(x) paste(x,day_lags,sep="_")))
+variables <- c(weather_vars_lags, )
+
+
+# formula <- reformulate(termlabels=weather_vars_lags,
+#                        response='value')
+# formula_vars <- vars(all.vars(formula))
+
+# Adding lag
+meas_weather_lag <- meas_weather %>% rowwise() %>%
+  mutate(meas_weather=list(utils.add_lag(meas_weather, weather_vars, group_cols=c(), day_lags, 'day')))
+
+
+### Result folder
+# Create results folder
+timestamp_str <- format(Sys.time(), "%Y%m%d_%H%M%S")
+result_folder <- file.path(output_folder, paste(timestamp_str,exp_name,sep='_'))
+dir.create(result_folder)
+
+# Save script file
+file.copy(org_file, result_folder, overwrite = T)
+file.rename(from = file.path(result_folder, basename(org_file)),
+            to = file.path(result_folder, paste0(timestamp_str, '_', tools::file_path_sans_ext(basename(org_file)),'.R')))
+
+# Train models
+train_row <- function(station_id, data, variables){
+  tryCatch({
+    data_prepared <- data %>% 
+      mutate(date=as.POSIXct(date)) %>%
+      rmw_prepare_data(na.rm = TRUE)
+    
+    rmw_do_all(
+      data_prepared,
+      variables = c("date_unix", "day_julian", "weekday",variables),
+      n_trees = 300,
+      n_samples = 300,
+      verbose = TRUE
+    )
+  }, error=function(err){
+    warning(paste("Station id failed:",station_id,':',err))
+    return(NA)})
+}
+
+nworkers <- 1#as.integer(future::availableCores() - 1)
+models_fitted <- pbmcmapply(train_row,
+                            station_id=meas_weather_lag$station_id,
+                            data=meas_weather_lag$meas_weather,
+                            variables=weather_vars_lags,
+                            mc.cores=nworkers,
+                            SIMPLIFY=FALSE)
+
+meas_weather$model_fitted <- unname(models_fitted)
+models_fitted <- NULL
