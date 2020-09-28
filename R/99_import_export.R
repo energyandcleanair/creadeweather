073331dfef57ecba8c3749016e06f20f427acccd
Hubert Thieriot <ht@erprofiler.com>
2020-09-28 15:12:23 +0800

    -missing import

--- R/99_import_export.R
+++ R/99_import_export.R
@@ -2,7 +2,7 @@
 #'
 #' @importFrom dplyr select rename mutate filter filter_at arrange distinct summarise 
 #'     do group_by group_by_at ungroup rowwise do left_join inner_join everything bind_rows 
-#'     pull tibble as_tibble rowwise any_vars all_vars
+#'     pull tibble as_tibble rowwise any_vars all_vars vars
 NULL
 
 
