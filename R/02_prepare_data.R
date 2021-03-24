1935ec7cc64e06a6079524b02f96393c8842a5dc
Hubert Thieriot <hubert.thieriot@gmail.com>
2021-03-24 08:15:58 +0000

    -cleaner data preparation

--- R/02_prepare_data.R
+++ R/02_prepare_data.R
@@ -6,19 +6,10 @@
 #' @export
 #'
 #' @examples
-prep_data <- function(meas_weather, filename=NULL){
-  
-  cache_folder <- file.path('data', '02_prep_training', 'cache')
-  if(!dir.exists(cache_folder)) dir.create(cache_folder, recursive = T)
-  
-  output_folder <- file.path('data', '02_prep_training', 'output')
-  if(!dir.exists(output_folder)) dir.create(output_folder, recursive = T)
-  
-  input_folder <- file.path('data', '02_prep_training', 'input')
-  if(!dir.exists(input_folder)) dir.create(input_folder, recursive = T)
+prep_data <- function(meas_weather){
   
   # Filter outliers etc.
-  clean_meas_tbl <- function(tbl){
+  clean_data <- function(tbl){
     
     # Remove infs in all vars
     tbl <- tbl %>% dplyr::mutate_if(is.numeric, function(x) ifelse(is.infinite(x), NA, x))
@@ -32,73 +23,67 @@ prep_data <- function(meas_weather, filename=NULL){
     if(n_before!=n_after){
       print(paste(n_before-n_after,"/", n_before,"outliers removed. Threshold:", n_mad*mad+median(tbl$value)))
     }
+    
+    # Replace NaNs with NA (gbm doesn't like NaNs)
+    tbl <- tbl %>% utils.replace_nan_with_na()
+    
     return(tbl)
   }
   
   
   # Certain stations miss certain weather variables
-  coalesce_weather_tbl <- function(tbl){
+  fill_data <- function(tbl){
+    
+    weather_cols <- setdiff(names(tbl), c("date","timezone","value"))
     
     # Different kind of treatments
-    # We only coalesce weather data where other weather data exists
-    weather_rows_idxs <- which(apply(tbl %>% dplyr::select(wd, precip, ceil_hgt, atmos_pres, wd, visibility, air_temp), 1, function(x) any(!is.na(x))))
-  
-    if(length(weather_rows_idxs)>0){
-      
-      # If only some of them are NAs
-      max_gap = 5
-      try(tbl <- tbl %>% mutate(sunshine=zoo::na.approx(sunshine, date, na.rm=FALSE, maxgap=max_gap)) %>% ungroup(), TRUE)
-      try(tbl <- tbl %>% mutate(atmos_pres=zoo::na.approx(atmos_pres, date, na.rm=FALSE, maxgap=max_gap)) %>% ungroup(), TRUE)
-      try(tbl <- tbl %>% mutate(air_temp_min=zoo::na.approx(air_temp_min, date, na.rm=FALSE, maxgap=max_gap)) %>% ungroup(), TRUE)
-      try(tbl <- tbl %>% mutate(air_temp_max=zoo::na.approx(air_temp_max, date, na.rm=FALSE, maxgap=max_gap)) %>% ungroup(), TRUE)
-      try(tbl <- tbl %>% mutate(air_temp=zoo::na.approx(air_temp, date, na.rm=FALSE, maxgap=max_gap)) %>% ungroup(), TRUE)
-      
-      if('pbl_min' %in% colnames(tbl)) try(tbl <- tbl %>% mutate(pbl_min=zoo::na.approx(pbl_min, date, na.rm=FALSE, maxgap=max_gap)) %>% ungroup(), TRUE)
-      if('pbl_max' %in% colnames(tbl)) try(tbl <- tbl %>% mutate(pbl_max=zoo::na.approx(pbl_max, date, na.rm=FALSE, maxgap=max_gap)) %>% ungroup(), TRUE)
-      
-      
-      
-      # If all NAs
-      fill_value <- function(v){
-        # if(all(is.na(v)) | ! (0 %in% unique(v))) v[is.na(v)] <- 0
-        if(all(is.na(v))) v[is.na(v)] <- 0
-        v
-      }
-
-      tbl <- tbl %>% mutate_at(c("ws","precip","ceil_hgt",
-                                 "atmos_pres","wd","visibility",
-                                 "air_temp_min","air_temp_max","air_temp"),
-                               fill_value)
-      
-      if('sunshine' %in% colnames(tbl)) tbl$sunshine <- fill_value(tbl$sunshine)
-      
-      
-      
-      # tbl[weather_rows_idxs,]$precip <- coalesce(tbl[weather_rows_idxs,]$precip, 0) # Precip is a bit special
+    
+    # Treatment 1: If all NA, then replace with 0
+    # We do it for all variables
+    vars_allnatozero <- weather_cols
+    allnatozero <- function(v){
+      if(all(is.na(v))) v[is.na(v)] <- 0
+      v
     }
+    
+    tbl <- tbl %>% mutate_at(intersect(vars_allnatozero, weather_cols),
+                             allnatozero)
+    
+    # Treatment 2: Replace NA with 0
+    # We do it only for precipitation
+    vars_natozero <- c("precip")
+    natozero <- function(v){
+      v[is.na(v)] <- 0
+      v
+    }
+    tbl <- tbl %>% mutate_at(intersect(vars_natozero, weather_cols),
+                             natozero)
+    
+    
+    # Treatment 3: interpolate missing values
+    max_gap = 5
+    vars_interpolate <- c("sunshine", "atmos_pres", "air_temp_min", "air_temp_max", "air_temp", "ceil_hgt", "pbl_min", "pbl_max")
+    interpolate <- function(v, date){
+      zoo::na.approx(v, date, na.rm=FALSE, maxgap=max_gap)
+    }
+    
+    tbl <- tbl %>% mutate_at(intersect(vars_interpolate, weather_cols),
+                             ~interpolate(.x,date=date))
     
     return(tbl)
   }
   
   # Add wind direction factor
-  enrich_weather_tbl <- function(tbl){
+  enrich_data <- function(tbl){
     tbl$wd_factor <- factor(tbl$wd %/% 45)
     return(tbl)
   }
   
   meas_weather <- meas_weather %>% dplyr::rowwise() %>%
-    mutate(meas_weather=list(clean_meas_tbl(meas_weather))) %>%
-    mutate(meas_weather=list(coalesce_weather_tbl(meas_weather))) %>%
-    mutate(meas_weather=list(enrich_weather_tbl(meas_weather)))
+    mutate(meas_weather=list(clean_data(meas_weather))) %>%
+    mutate(meas_weather=list(fill_data(meas_weather))) %>%
+    mutate(meas_weather=list(enrich_data(meas_weather)))
  
-  # Replace NaNs with NA (gbm doesn't like NaNs)
-  meas_weather <- meas_weather %>% rowwise() %>%
-    mutate(meas_weather=list(utils.replace_nan_with_na(meas_weather)))
-  
-  if(!is.null(filename)){
-    saveRDS(meas_weather, file.path(output_folder, filename))  
-  }
-
   return(meas_weather)
 }
 
