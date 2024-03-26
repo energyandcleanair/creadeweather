61e5f891b64834849d6df61c4c9c604959444b16
Danny Hartono <danny.hartono@outlook.com>
2024-03-26 02:50:36 +0000

    - add utils.check_atmos_press to weather - change default threshold to 0.9

--- R/99_utils.R
+++ R/99_utils.R
@@ -320,9 +320,9 @@ utils.get_env <- function(x, error_if_not_found=F){
 }
 
 
-utils.check_atmos_press <- function(weather){
+utils.check_atmos_press <- function(weather, threshold = 0.9){
   check <- weather[['weather']][[1]] %>% filter(is.na(atmos_pres)) %>% nrow() > 
-    (nrow(weather[['weather']][[1]]) * 0.7)
+    (nrow(weather[['weather']][[1]]) * threshold)
   
   if(check) weather[['weather']][[1]] <- weather[['weather']][[1]] %>% select(-atmos_pres)
   
